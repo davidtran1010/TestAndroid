b0d1a2101857825302373b9edc05cd94b5e24cde
tatbt <tatbt2010@gmail.com>
2017-02-16 09:57:23 +0700

    GRDGZD

--- app/src/main/java/com/tatbt/sudoku/MainActivity.java
+++ app/src/main/java/com/tatbt/sudoku/MainActivity.java
@@ -10,12 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_main);
         
-        setContentView(R.layout.activity_main);
-        
-        setContentView(R.layout.activity_main); //fsdgvsfgbfdbgdf
+        /// test this line gzdfbdfbzd
     }
 }
